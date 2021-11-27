0628f96b49ab5ed9d7d037f5511c3731b219775a
Zheka001 <aidarkinzhenya@mail.ru>
2021-11-27 04:22:27 +0300

    feat: update plots

--- src/visualizer.py
+++ src/visualizer.py
@@ -7,21 +7,34 @@ from pathlib import Path
 
 
 class Visualizer:
-    def __init__(self, path_to_file):
-        self.path_to_file = Path(path_to_file)
-
-    def run(self):
-        data = self._read_json(self.path_to_file)
-        df = self._preprocess_data(data)
-        self._draw_image(df)
+    def __init__(self, path_to_save):
+        self.path_to_save = Path(path_to_save)
 
     def combine(self):
+        sns.set_theme(style="darkgrid")
+        fig, ax = plt.subplots(1, 2, figsize=[15, 6])
         data = self._read_json(Path('data/random_results_500.json'))
         df = self._preprocess_data(data)
-        self._draw_image(df)
         data = self._read_json(Path('data/random_results_identity_500.json'))
+        df_identity = self._preprocess_data(data)
+        df_identity.columns = [name + '_identity' for name in df_identity.columns]
+        common_df = pd.concat([df, df_identity], axis=1)
+
+        self._draw_image(common_df, ax=ax[0], title='Равномерные стирания',
+                         labels=['Равномерные стирания', 'Группирующиеся стирания'])
+        self._draw_image2(common_df, ax=ax[1], title='Группирующиеся стирания')
+
+    def pair_plot(self, info):
+        sns.set_theme(style="darkgrid")
+        fig, ax = plt.subplots(1, 2, figsize=[15, 6])
+        data = self._read_json(Path(info['file1']))
         df = self._preprocess_data(data)
-        self._draw_image2(df)
+        self._draw_image(df, ax=ax[0], title=info['title1'], labels=info['labels1'])
+        data = self._read_json(Path(info['file2']))
+        df = self._preprocess_data(data)
+        self._draw_image(df, ax=ax[1], title=info['title2'], labels=info['labels2'])
+
+        plt.savefig(self.path_to_save)
 
     @staticmethod
     def _read_json(path_to_file):
@@ -36,35 +49,61 @@ class Visualizer:
         df['group'] = 1 - df['group']
         df['simple'] = 1 - df['simple']
         # усреднение
-        kernel = np.ones(kernel_size) / kernel_size
-        df['group erasures'] = np.convolve(df['group'], kernel, mode='same')
-        df['simple erasures'] = np.convolve(df['simple'], kernel, mode='same')
+        # kernel = np.ones(kernel_size) / kernel_size
+        # group_array = np.pad(df['group'], (kernel_size // 2, kernel_size // 2 - 1), mode='edge')
+        # simple_array = np.pad(df['simple'], (kernel_size // 2, kernel_size // 2 - 1), mode='edge')
+        # df['group erasures'] = np.convolve(group_array, kernel, mode='valid')
+        # df['simple erasures'] = np.convolve(simple_array, kernel, mode='valid')
+        df['group erasures'] = df['group']
+        df['simple erasures'] = df['simple']
         return df
 
-    def _draw_image(self, df, need_new_plot=True):
-        if need_new_plot:
-            plt.figure(figsize=[12, 6])
+    def _draw_image(self, df, ax=None, need_new_plot=False, title=None, labels=None):
         sns.set_theme(style="darkgrid")
-        fig = sns.lineplot(data=df, x='probability', y='simple erasures', label='простой тип стираний', color='black', linestyle='--')
-        fig = sns.lineplot(data=df, x='probability', y='group erasures', label='групповой тип стираний', color='black', linestyle='solid')
-        fig = sns.lineplot(np.linspace(0.0, 0.5), np.linspace(0.0, 0.5), label='передача чистой информации', color='black', linestyle='-.')
-        fig.set(xlabel='Вероятность получения стирания',
+        fig = sns.lineplot(data=df, x='probability', y='simple erasures', label=labels[0], color='black', linestyle='--', ax=ax)
+        fig = sns.lineplot(data=df, x='probability', y='group erasures', label=labels[1], color='black', linestyle='solid', ax=ax)
+        # fig = sns.lineplot(np.linspace(0.0, 0.5), np.linspace(0.0, 0.5), label=labels[2], color='black', linestyle='-.', ax=ax)
+        fig.set(title=title,
+                xlabel='Вероятность получения стирания',
+                ylabel='Вероятность ошибки декодирования',
+                xlim=[-0.01, 0.61],
+                ylim=[-0.01, 0.61])
+        # plt.savefig(f'{self.path_to_file.with_suffix(".png")}')
+
+    @staticmethod
+    def _draw_image2(df, ax=None, title=None):
+        sns.set_theme(style="darkgrid")
+        fig = sns.lineplot(data=df, x='probability', y='group erasures', label='метод равновесных столбцов', color='black', linestyle='--', ax=ax)
+        fig = sns.lineplot(data=df, x='probability', y='group erasures_identity', label='модификация метода равновесных столбцов', color='black', linestyle='solid', ax=ax)
+        fig.set(title=title,
+                xlabel='Вероятность получения стирания',
                 ylabel='Вероятность ошибки декодирования',
                 xlim=[-0.01, 0.51],
                 ylim=[-0.01, 0.51])
-        plt.savefig(f'{self.path_to_file.with_suffix(".png")}')
-
-    @staticmethod
-    def _draw_image2(df):
-        fig = sns.lineplot(data=df, x='probability', y='simple erasures', label='простой тип стираний, модификация', color='black', linestyle='solid')
-        fig = sns.lineplot(data=df, x='probability', y='group erasures', label='групповой тип стираний, модификация', color='black', linestyle='solid')
-        plt.savefig('data/combined_group.png')
+        plt.savefig('data/figure_2.png')
 
 
 if __name__ == '__main__':
-    json_files = Path('data').glob('*.json')
-    for json_file in json_files:
-        vis = Visualizer(json_file)
-        vis.run()
+    # Visualizer(Path('data/random_results_2.png')).combine()
 
-    # Visualizer(Path('data/random_results_500.json')).combine()
+    cfg1 = {
+        'file1': 'data/random_results_500.json',
+        'title1': 'Метод равновесных столбцов',
+        'labels1': ['равномерные стирания', 'группирующиеся стирания', 'передача данных без кодирования'],
+
+        'file2': 'data/random_results_identity_500.json',
+        'title2': 'Модицицированный метод равновесных столбцов',
+        'labels2': ['равномерные стирания', 'группирующиеся стирания', 'передача данных без кодирования'],
+    }
+
+    cfg2 = {
+        'file1': 'data/random_results_12_6_old.json',
+        'title1': 'Метод равновесных столбцов с параметрами (6, 12)',
+        'labels1': ['равномерные стирания', 'группирующиеся стирания', 'передача данных без кодирования'],
+
+        'file2': 'data/random_results_14_7.json',
+        'title2': 'Метод равновесных столбцов с параметрами (7, 14)',
+        'labels2': ['равномерные стирания', 'группирующиеся стирания', 'передача данных без кодирования'],
+    }
+
+    Visualizer(Path('data/figure_3.png')).pair_plot(cfg1)
